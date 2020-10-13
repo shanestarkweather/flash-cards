36bf9c3c65c72abae6e1b49aec234bcd86229538
shanestarkweather <shane@shanestarkweather.com>
2020-10-13 12:21:43 -0700

    Change to arrays to object array

--- script.js
+++ script.js
@@ -1,56 +1,184 @@
-const equations = [
-	'1 + 1',
-	'3 + 2',
-	'4 + 2',
-	'1 + 6',
-	'3 + 2',
-	'5 + 4',
-	'8 + 1',
-	'1 + 2',
-	'3 + 1',
-	'1 + 5',
-	'4 + 6',
-	'7 + 1',
-	'1 + 3',
-	'2 + 4',
-	'2 + 6',
-	'5 + 2',
-	'1 + 0',
-	'4 + 4',
-	'3 + 5',
-	'0 + 9',
-	'8 + 2',
-	'2 + 2',
-	'3 + 4',
-	'5 + 5',
-	'4 + 1',
-];
-const answers = [
-	'2',
-	'5',
-	'6',
-	'7',
-	'5',
-	'9',
-	'9',
-	'3',
-	'4',
-	'6',
-	'10',
-	'8',
-	'4',
-	'6',
-	'8',
-	'7',
-	'1',
-	'8',
-	'8',
-	'9',
-	'10',
-	'4',
-	'7',
-	'10',
-	'5',
+// const equations = [
+// 	'1 + 1',
+// 	'3 + 2',
+// 	'4 + 2',
+// 	'1 + 6',
+// 	'3 + 2',
+// 	'5 + 4',
+// 	'8 + 1',
+// 	'1 + 2',
+// 	'3 + 1',
+// 	'1 + 5',
+// 	'4 + 6',
+// 	'7 + 1',
+// 	'1 + 3',
+// 	'2 + 4',
+// 	'2 + 6',
+// 	'5 + 2',
+// 	'1 + 0',
+// 	'4 + 4',
+// 	'3 + 5',
+// 	'0 + 9',
+// 	'8 + 2',
+// 	'2 + 2',
+// 	'3 + 4',
+// 	'5 + 5',
+// 	'4 + 1',
+// ];
+// const answers = [
+// 	'2',
+// 	'5',
+// 	'6',
+// 	'7',
+// 	'5',
+// 	'9',
+// 	'9',
+// 	'3',
+// 	'4',
+// 	'6',
+// 	'10',
+// 	'8',
+// 	'4',
+// 	'6',
+// 	'8',
+// 	'7',
+// 	'1',
+// 	'8',
+// 	'8',
+// 	'9',
+// 	'10',
+// 	'4',
+// 	'7',
+// 	'10',
+// 	'5',
+// ];
+
+const equationArray = [
+	{
+		equation: '1 + 1',
+		answer: '2',
+		correct: false,
+	},
+	{
+		equation: '3 + 2',
+		answer: '5',
+		correct: false,
+	},
+	{
+		equation: '4 + 2',
+		answer: '6',
+		correct: false,
+	},
+	{
+		equation: '1 + 6',
+		answer: '7',
+		correct: false,
+	},
+	{
+		equation: '0 + 1',
+		answer: '1',
+		correct: false,
+	},
+	{
+		equation: '5 + 4',
+		answer: '9',
+		correct: false,
+	},
+	{
+		equation: '8 + 1',
+		answer: '9',
+		correct: false,
+	},
+	{
+		equation: '1 + 2',
+		answer: '3',
+		correct: false,
+	},
+	{
+		equation: '3 + 1',
+		answer: '4',
+		correct: false,
+	},
+	{
+		equation: '1 + 5',
+		answer: '6',
+		correct: false,
+	},
+	{
+		equation: '4 + 6',
+		answer: '10',
+		correct: false,
+	},
+	{
+		equation: '7 + 1',
+		answer: '8',
+		correct: false,
+	},
+	{
+		equation: '1 + 3',
+		answer: '4',
+		correct: false,
+	},
+	{
+		equation: '2 + 4',
+		answer: '6',
+		correct: false,
+	},
+	{
+		equation: '2 + 6',
+		answer: '8',
+		correct: false,
+	},
+	{
+		equation: '5 + 2',
+		answer: '7',
+		correct: false,
+	},
+	{
+		equation: '1 + 0',
+		answer: '0',
+		correct: false,
+	},
+	{
+		equation: '4 + 4',
+		answer: '8',
+		correct: false,
+	},
+	{
+		equation: '3 + 5',
+		answer: '8',
+		correct: false,
+	},
+	{
+		equation: '0 + 9',
+		answer: '9',
+		correct: false,
+	},
+	{
+		equation: '8 + 2',
+		answer: '10',
+		correct: false,
+	},
+	{
+		equation: '2 + 2',
+		answer: '4',
+		correct: false,
+	},
+	{
+		equation: '3 + 4',
+		answer: '7',
+		correct: false,
+	},
+	{
+		equation: '5 + 5',
+		answer: '10',
+		correct: false,
+	},
+	{
+		equation: '4 + 1',
+		answer: '5',
+		correct: false,
+	},
 ];
 
 const card = document.querySelector('#flash-card');
@@ -63,17 +191,17 @@ const resetButton = document.querySelector('#reset');
 
 let count = 0;
 
-card.innerText = equations[0];
+card.innerText = equationArray[0].equation;
 let equationShowing = true;
 
 function showAnswer() {
 	if (equationShowing) {
 		equationShowing = false;
-		const answer = answers[count];
+		const answer = equationArray[count].answer;
 		card.innerText = answer;
 	} else {
 		equationShowing = true;
-		card.innerText = equations[count];
+		card.innerText = equationArray[count].equation;
 	}
 }
 
@@ -95,7 +223,7 @@ function addCorrect() {
 
 function correctScore() {
 	numberCorrect++;
-	correct.innerText = `${numberCorrect} of ${equations.length} cards`;
+	correct.innerText = `${numberCorrect} of ${equationArray.length} cards`;
 	nextCard();
 }
 
@@ -118,37 +246,41 @@ document.addEventListener('keydown', function (event) {
 
 function nextCard() {
 	// How do I stop this at 25?
-	card.className = 'ungraded'
-	if (count === equations.length - 1) {
+	card.className = 'ungraded';
+	// if (count <= equations.length-1) {
+
+	// }
+
+	if (count === equationArray.length - 1) {
 		count = 0;
-	} else if (count <= equations.length) {
+	} else if (count <= equationArray.length) {
 		count++;
 	}
-	card.innerText = equations[count];
-	cardNumber.innerText = `${count + 1} of ${equations.length} cards`;
+	card.innerText = equationArray[count].equation;
+	cardNumber.innerText = `${count + 1} of ${equationArray.length} cards`;
 }
 
 function previousCard() {
 	if (count !== 0) {
 		count--;
 	}
-	card.innerText = equations[count];
-	cardNumber.innerText = `${count + 1} of ${equations.length} cards`;
+	card.innerText = equationArray[count].equation;
+	cardNumber.innerText = `${count + 1} of ${equationArray.length} cards`;
 }
 
 previousButton.addEventListener('click', previousCard);
 
-cardNumber.innerText = `${count + 1} of ${equations.length} cards`;
+cardNumber.innerText = `${count + 1} of ${equationArray.length} cards`;
 
 let numberCorrect = 0;
 
-correct.innerText = `${numberCorrect} of ${equations.length} cards`;
+correct.innerText = `${numberCorrect} of ${equationArray.length} cards`;
 
 function resetDeck() {
 	count = 0;
 	numberCorrect = 0;
-	card.innerText = equations[0];
-	correct.innerText = `${numberCorrect} of ${equations.length} cards`;
-	cardNumber.innerText = `${count + 1} of ${equations.length} cards`;
+	card.innerText = equationArray[0];
+	correct.innerText = `${numberCorrect} of ${equationArray.length} cards`;
+	cardNumber.innerText = `${count + 1} of ${equation.length} cards`;
 }
 resetButton.addEventListener('click', resetDeck);
